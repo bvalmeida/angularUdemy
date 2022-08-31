b56d29ab222859daeb064d279995a973be6cbae1
bvalmeida <bvalmeida94@gmail.com>
2022-08-31 20:57:03 -0300

    aprendendo sobre compilação de typescript

---
+++ angular/typescript/build/tipado.js
@@ -0,0 +1,9 @@
+// Boolean
+let eValido = true;
+eValido = false;
+// Number
+let idade = 12;
+// String
+let nome = "Breno";
+let apelido = "Vieira";
+let nomeCompleto = `${nome} ${apelido}`;
---
+++ angular/typescript/tipado.ts
@@ -0,0 +1,12 @@
+// Boolean
+let eValido: boolean = true;
+eValido = false;
+
+// Number
+let idade: number = 12;
+
+// String
+let nome: string = "Breno";
+let apelido: string = "Vieira";
+
+let nomeCompleto: string = `${nome} ${apelido}`;
