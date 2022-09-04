3886bd5d2a50ab10fcdeea69868230da1d5cd2da
bvalmeida <bvalmeida94@gmail.com>
2022-09-04 08:37:30 -0300

    continuando com o aprendizado

--- angular/typescript/build/tipado.js
+++ angular/typescript/build/tipado.js
@@ -7,3 +7,26 @@ let idade = 12;
 let nome = "Breno";
 let apelido = "Vieira";
 let nomeCompleto = `${nome} ${apelido}`;
+// Array
+let numeros = [12, 3, 4, 5, 6, 7];
+let numeros2 = [3, 4, 5, 6, 7];
+// Dupla
+let sitio = ['Casa', 1243];
+// Enum de forma automática
+var Estado;
+(function (Estado) {
+    Estado[Estado["Offline"] = 0] = "Offline";
+    Estado[Estado["Indefinido"] = 1] = "Indefinido";
+    Estado[Estado["Online"] = 2] = "Online";
+})(Estado || (Estado = {}));
+// Ou desta outra forma
+var Estado2;
+(function (Estado2) {
+    Estado2[Estado2["Offline"] = 1] = "Offline";
+    Estado2[Estado2["Indefinido"] = 0] = "Indefinido";
+    Estado2[Estado2["Online"] = -1] = "Online";
+})(Estado2 || (Estado2 = {}));
+let stat = Estado.Online;
+let stat2 = Estado2.Online;
+console.log(stat2);
+console.log(stat);
--- angular/typescript/tipado.ts
+++ angular/typescript/tipado.ts
@@ -10,3 +10,31 @@ let nome: string = "Breno";
 let apelido: string = "Vieira";
 
 let nomeCompleto: string = `${nome} ${apelido}`;
+
+// Array
+let numeros: number[] = [12, 3, 4, 5, 6, 7];
+let numeros2: Array<number> = [3, 4, 5, 6, 7];
+
+// Dupla
+let sitio: [string, number] = ['Casa', 1243];
+
+// Enum de forma automática
+enum Estado {
+    Offline,
+    Indefinido,
+    Online
+}
+
+// Ou desta outra forma
+enum Estado2 {
+    Offline = 1,
+    Indefinido = 0,
+    Online = -1
+}
+
+let stat: Estado = Estado.Online;
+let stat2: Estado2 = Estado2.Online;
+
+console.log(stat2);
+console.log(stat);
+
