0ffb732d95f3f745484f13ddfa0b81c9a3c71812
bvalmeida <bvalmeida94@gmail.com>
2023-01-17 19:53:52 -0300

    impl @output

--- angular/primeiroapp/src/app/alerta/alerta.component.ts
+++ angular/primeiroapp/src/app/alerta/alerta.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alerta',
@@ -10,14 +10,21 @@ export class AlertaComponent implements OnInit {
   @Input('texto') mensagem: string;
   @Input() subTitulo: string;
 
+  @Output() alertaApertado: EventEmitter<string>;
+
   constructor() {
     this.mensagem = 'Mensagem de alerta está com defeito';
     this.subTitulo = 'Valor do subTítulo';
+    this.alertaApertado = new EventEmitter();
     //console.log('Vindo do Construtor: ' + this.mensagem);
   }
 
   ngOnInit(): void {
     //console.log('Vindo do OnInit: ' + this.mensagem);
+  }
+
+  onClick(){
+    this.alertaApertado.emit('O alerta foi apertado!!');
   }
 
 }
--- angular/primeiroapp/src/app/app.component.ts
+++ angular/primeiroapp/src/app/app.component.ts
@@ -14,4 +14,8 @@ export class AppComponent {
   }
 
   title = 'PrimeiroApp';
+
+  onAlertaApertado($event){
+    console.log($event);
+  }
 }
