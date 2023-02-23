4f678d018aae6d6ff028075f56190a128ed9b413
bvalmeida <bvalmeida94@gmail.com>
2023-02-23 20:04:01 -0300

    ngclass e ngif

--- angular/directivas-project/src/app/app.component.ts
+++ angular/directivas-project/src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   propriedadesParagrafos: any;
+
+  mostrar: boolean;
 
   constructor(){
     this.propriedadesParagrafos = {
       color: 'red',
       fontSize: '24px'
     }
+
+    this.mostrar = false;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onClickMostrar(){
+    this.mostrar = !this.mostrar;
   }
 
   mudarCor(pColor: string){
--- angular/directivas-project/src/app/semaforo/semaforo.component.ts
+++ angular/directivas-project/src/app/semaforo/semaforo.component.ts
@@ -7,14 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SemaforoComponent implements OnInit {
 
+  arrayClasses: string[];
+
   cadeiasClasses: string;
 
+  corSelecionado: string;
+
   constructor(){
-    this.cadeiasClasses = 'roxo';
+    this.cadeiasClasses = 'semaforo roxo';
+
+    this.arrayClasses = ['semaforo', 'roxo', 'redondo'];
+
+    this.corSelecionado = 'v';
   }
 
   ngOnInit(): void{
-
+    setInterval(() =>{
+      if(this.corSelecionado === 'r'){
+        this.corSelecionado = 'a';
+      }else if(this.corSelecionado === 'a'){
+        this.corSelecionado = 'v';
+      }else if(this.corSelecionado === 'v'){
+        this.corSelecionado = 'r'
+      }
+    }, 1000);
   }
 
 
